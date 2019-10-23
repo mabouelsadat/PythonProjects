cabc76be72f704607e7bbbb43a0ef4ca1f91e26c
Mohamed Abouelsaadat <m.abouelsadat@gmail.com>
2019-10-23 13:12:41 +0200

    Charachter Input replaced

--- CharacterInput.py
+++ CharacterInput.py
@@ -1,2 +1,44 @@
-name = input("Give me your name: ")
-print("Your name is " + name)
+x = 200
+y = 200.1
+is_Signed_In = True
+
+print (type(is_Signed_In))
+
+print(" this is parsing shit " + str(x))
+
+email = """Dear Sender
+
+Your email has been registered. 
+
+regards, 
+Support Team 
+"""
+
+print(len(email))
+print(email[1:-2])
+print(email.upper())
+
+#replacing
+print(email.replace("Lorem", "400"))
+
+#Splitting
+print(email.split())
+
+#in
+
+print("Dear"  in email )
+
+name = "Mohamed"
+lastname = "Abouelsaadat"
+email = "m.abouelsadat@gmail.com"
+
+
+Email_To_Sender = """Dear {} {}
+
+Your email {} has been registered. 
+
+regards, 
+Support Team 
+"""
+
+print(Email_To_Sender.format(name , lastname, email))
